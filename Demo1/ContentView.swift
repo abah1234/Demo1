88f45405d80df2e5eb35c016b58379f402eca36d
pien <ywww301@gmail.com>
2020-10-14 14:22:39 +0800

    Initial Commit

---
+++ Demo1/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Demo1
+//
+//  Created by 蔡文彬 on 2020/10/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Demo1/Demo1App.swift
@@ -0,0 +1,17 @@
+//
+//  Demo1App.swift
+//  Demo1
+//
+//  Created by 蔡文彬 on 2020/10/14.
+//
+
+import SwiftUI
+
+@main
+struct Demo1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
